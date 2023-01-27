e3cd116c17bdc3f217eed41aa3f1a86624972c72
petko <p.todorov1015@gmail.com>
2023-01-27 19:19:13 +0200

    1

--- main.py
+++ main.py
@@ -17,15 +17,6 @@ while game_running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 menu_ = True
-    #     elif event.type == pygame.MOUSEBUTTONDOWN:
-    #         # Check if the mouse cursor is within the button's rectangle
-    #         if button_rect_quit.collidepoint(event.pos):
-    #             quit()
-    # screen.blit(button_quit, button_rect_quit)
-    # screen.blit(title_quit, (button_rect_quit.x + 50, button_rect_quit.y + 17))
-    #
-    # screen.blit(button_quit, button_rect_play)
-    # screen.blit(title_play, (button_rect_play.x + 50, button_rect_play.y + 17))
     pygame.display.flip()
 
 pygame.quit()
--- menu_function.py
+++ menu_function.py
@@ -1,23 +1,36 @@
 import pygame
 
+buttons_x, buttons_y = 1920 / 2 - 200, 1080 / 2 - 150
 menu_image = pygame.image.load('images/menu/menu.jpg')
+
 button_play = pygame.image.load('images/menu/play.png')
 button_play_rect = button_play.get_rect()
-button_play_rect.move_ip(850, 400)
+button_play_rect.x = buttons_x
+button_play_rect.y = buttons_y
+
+button_quit = pygame.image.load('images/menu/exit.png')
+button_quit_rect = button_quit.get_rect()
+button_quit_rect.x = buttons_x
+button_quit_rect.y = buttons_y + 100
+
+print(button_play_rect)
 
 
 def menu_func(running_menu, screen):
+    global mouse_pos
     while running_menu:
         screen.blit(menu_image, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return False
+                    quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if button_play_rect.collidepoint(event.pos):
-                    pass
+                mouse_pos = pygame.mouse.get_pos()
+                if button_quit_rect.collidepoint(mouse_pos):
+                    quit()
 
-        pygame.draw.rect(screen, (255, 0, 0), button_play_rect, 10)
-        screen.blit(button_play, (800, 400))
+        screen.blit(button_play, button_play_rect)
+        screen.blit(button_quit, button_quit_rect)
         pygame.display.flip()
+
         # print(button_quit.get_rect())
