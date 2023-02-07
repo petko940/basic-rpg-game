92b677d56d864aa2c64564cef710598a574827af
kumchovylcho <tamerchoyy@gmail.com>
2023-02-08 01:57:50 +0200

    added the map controller, now forest map is visible

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pygame
 
 from menu_class import Menu
 from warrior_character import Warrior
+from class_maps.map_controller import MapController
 
 pygame.init()
 
 WIDTH, HEIGHT = (1920, 1080)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+map_controller = MapController()
+map_controller.create_map([pygame.image.load(f'images/maps/map1/({i}).png') for i in range(1, 5 + 1)], "Forest")
+
 
 menu = Menu()
 game_running = True
@@ -16,19 +21,20 @@ a = pygame.Rect(0, 0, 1920, 1080)
 warrior = Warrior()
 while game_running:
     # menu.menu()
+    screen.blit(map_controller.show_current_map(), (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 game_running = False
             # elif event.key == pygame.K_d:
             #     screen.blit(warrior.walk_images("right"),warrior.idle_animation("right").get_rect())
-                # TO DO walk right
-                # char.walking = True
+            # TO DO walk right
+            # char.walking = True
     if pygame.key.get_pressed()[pygame.K_d]:
         screen.blit(warrior.walk_images("right"), warrior.idle_animation("right").get_rect())
         pygame.display.flip()
 
-    screen.blit(warrior.idle_animation("right"),(100,100))
+    screen.blit(warrior.idle_animation("right"), (100, 100))
 
     pygame.display.flip()
 
