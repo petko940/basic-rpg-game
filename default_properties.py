665d0fffe70b0f0e2a5bf75a9bc0b0e26f83bfac
chebishev <atanas.chebishev@gmail.com>
2023-02-08 23:18:37 +0200

    added pickle insted of json in default_properties.py

--- default_properties.py
+++ default_properties.py
@@ -1,7 +1,26 @@
-import json
+import pygame
+import pickle
+from class_maps.map_controller import MapController
+from hunter_character import Hunter
+from mage_character import Mage
+from warrior_character import Warrior
 
 
-# import Map -> eventually to set the index to zero, if there is no other JSON files
+def load_data():
+    try:
+        with open("savegame", "rb") as file:
+            # file.seek(0)
+            take_from_file = pickle.load(file)
+    except FileNotFoundError:
+        return default_dictionary
+    return take_from_file
+
+
+def save_on_close(program_data: dict, map_data: MapController, current_hero: object):
+    program_data["Map"].current_map = map_data.current_map
+
+    with open('savegame', 'wb') as data:
+        pickle.dump(program_data, data)
 
 
 # default properties for the heroes
@@ -11,29 +30,26 @@ class Hero:
         self.power = power
         self.is_selected = is_selected
 
+    def get_name(self):
+        return self.name
+
     def set_selected(self):
         self.is_selected = True
         return self.is_selected
 
 
-class HeroEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Hero):
-            return obj.__dict__
-        return json.JSONEncoder.default(self, obj)
-
-
 hero = Hero("Superman", "super strength", True)
-
 first_hero = Hero("Dark Wizard", "fireballs", True)
 third_hero = Hero("Still no name", "warrior", False)
 
 default_dictionary = {
-    "Map": "",  # map-controller.current-map
-    "Hunter": Hero("Hunter", "Long range"),
-    "Mage": Hero("Mage", "Fireballs"),
-    "Warrior": Hero("Warrior", "Mad Axe")
+    "Map": MapController(),
+    "Hunter": Hunter(),
+    "Mage": Mage(),
+    "Warrior": Warrior()
 }
 
-with open("default.json", "w") as file:
-    file.write(json.dumps(default_dictionary, cls=HeroEncoder, indent=4))
+test = load_data()
+# todo  get info from object or fix EOFError
+print(test)
+save_on_close(default_dictionary, default_dictionary["Map"], default_dictionary["Hunter"])
