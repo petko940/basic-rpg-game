b827ec417ced85ab613337329ae94e09fe42cad6
kumchovylcho <tamerchoyy@gmail.com>
2023-02-09 13:40:37 +0200

    did some small changes on save_on_close function, and now it is ready for use

--- default_properties.py
+++ default_properties.py
@@ -8,19 +8,15 @@ from warrior_character import Warrior
 def load_data():
     try:
         with open("savegame", "rb") as file:
-            # file.seek(0)
             take_from_file = pickle.load(file)
     except FileNotFoundError:
         return default_dictionary
     return take_from_file
 
 
-def save_on_close(program_data: dict, map_data: MapController, current_hero: object):
+def save_on_close(program_data: dict, map_data: MapController, current_hero: (Mage, Warrior, Hunter)):
     program_data["Map"].current_map = map_data.current_map
-    # No idea if any of this is needed in order to work with the third argument:
-    # program_data["Mage"].index = mage.index
-    # program_data["Hunter"].index = hunter.index
-    # program_data["Warrior"].index = warrior.index
+    program_data[current_hero.__class__.__name__.capitalize()].index = current_hero.index
 
     with open('savegame', 'wb') as data:
         pickle.dump(program_data, data)
@@ -32,14 +28,16 @@ default_dictionary = {
     "Mage": Mage(),
     "Warrior": Warrior()
 }
-mage = Mage()
-map = MapController()
-test = load_data()
-
-save_on_close(default_dictionary, map, mage)
-print("##################################################")
-# print(f"from hunger_character this is the index of the Hunter: {test['Hunter'].index}")
-print(f"from mage_character this is the index of the Mage: {test['Mage'].index}")
-# print(f"from hunger_character this is the index of the Warrior: {test['Warrior'].index}")
-print(f"from map_controller this is the index of the current map: {test['Map'].current_map}")
-print("##################################################")
+# mage = Mage()
+# map = MapController()
+# test = load_data()
+#
+# print("##################################################")
+# # print(f"from hunger_character this is the index of the Hunter: {test['Hunter'].index}")
+# print(f"from mage_character this is the index of the Mage: {test['Mage'].index}")
+# # print(f"from hunger_character this is the index of the Warrior: {test['Warrior'].index}")
+# print(f"from map_controller this is the index of the current map: {test['Map'].current_map}")
+# print("##################################################")
+# print(test['Warrior'].jump_animation())
+#
+# save_on_close(default_dictionary, map, mage)
