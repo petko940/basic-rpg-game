85317984da8c478f1d36ed6d59fa8c7b90377dd4
kumchovylcho <tamerchoyy@gmail.com>
2023-02-09 12:57:27 +0200

    fixed bug where the game freezes for certain amount of time when loading screen

--- main.py
+++ main.py
@@ -26,16 +26,7 @@ is_right = True
 before_start = True
 
 while game_running:
-    if before_start:
-        screen.blit(current_hero.idle_animation('right'), actions.idle())
-        for i in range(510, 0, -1):
-            screen.blit(map_controller.show_current_map(), (0, 0))
-            screen.blit(current_hero.idle_animation('right'), actions.idle())
-            pygame.draw.rect(screen, (50, (255 - i // 2), 0), background_rect, int(i * 1.1))
-            pygame.display.flip()
-        before_start = False
 
-    screen.blit(map_controller.show_current_map(), (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
@@ -44,6 +35,16 @@ while game_running:
             #     screen.blit(warrior.walk_images("right"),warrior.idle_animation("right").get_rect())
             # TO DO walk right
             # char.walking = True
+
+    if before_start:
+        for i in range(510, 0, -1):
+            screen.blit(map_controller.show_current_map(), (0, 0))
+            screen.blit(current_hero.idle_animation('right'), actions.idle())
+            pygame.draw.rect(screen, (50, (255 - i // 2), 0), background_rect, int(i * 1.1))
+            pygame.display.flip()
+        before_start = False
+
+    screen.blit(map_controller.show_current_map(), (0, 0))
 
     if pygame.key.get_pressed()[pygame.K_d] and pygame.key.get_pressed()[pygame.K_SPACE]:
         screen.blit(current_hero.walk_images("right"), actions.walk())
