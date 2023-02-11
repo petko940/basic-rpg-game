09ff9a75e85e005dc81fa441ecac2b93536a7a04
kumchovylcho <tamerchoyy@gmail.com>
2023-02-11 23:35:29 +0200

    must inherit from Hero at the moment

---
+++ characters/hunter_character.py
@@ -0,0 +1,15 @@
+from pygame import image, transform
+
+
+class Hunter:
+
+    def __init__(self):
+        self.index = 0
+
+    # def idle_animation(self):
+    #     self.index += self.__IDLE_SPEED
+    #     return idle_images[int(self.index) % len(idle_images)]
+    #
+    # def jump_animation(self):
+    #     self.index += 0.1
+    #     return jump_images[int(self.index) % len(jump_images)]
