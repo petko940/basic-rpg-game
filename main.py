7bb2ad8c60dc3859a2e0629effe5d93fa2bef4ad
kumchovylcho <tamerchoyy@gmail.com>
2023-02-11 23:32:54 +0200

    made hero_controller object and now you can create warrior(for now), loading image function must be moved

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 
+from characters.hero_controller import HeroController
 from menu_class import Menu
-from warrior_character import Warrior
 from class_maps.map_controller import MapController
-from actions import Actions
+from characters.actions import Actions
 
 pygame.init()
 
@@ -18,15 +18,50 @@ def loading_game_screen(window, current_map: MapController, hero_actions: Action
 
 WIDTH, HEIGHT = (1366, 768)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-resize = 1.4
+resized = 1.4
+
+
+def load_mage_images():  # this func goes to hero_controller
+    attack_images = [pygame.image.load(f'images/mage/attack/({i}).png') for i in range(1, 11)]
+
+    die_images = [pygame.image.load(f'images/mage/die/({i}).png') for i in range(1, 11)]
+
+    idle_images = [pygame.image.load(f'images/mage/idle/({i}).png') for i in range(1, 11)]
+
+    jump_images = [pygame.image.load(f'images/mage/jump/({i}).png') for i in range(1, 11)]
+
+    walk_images = [pygame.image.load(f'images/mage/walk/({i}).png') for i in range(1, 11)]
+
+    return attack_images, die_images, idle_images, jump_images, walk_images
+
+
+def load_hunter_images():   # this func goes to hero_controller
+    attack_images = [pygame.image.load(f'images/hunt/attack/({i}).png') for i in range(1, 11)]
+
+    die_images = [pygame.image.load(f'images/hunt/die/({i}).png') for i in range(1, 11)]
+
+    idle_images = [pygame.image.load(f'images/hunt/idle/({i}).png') for i in range(1, 11)]
+
+    jump_images = [pygame.image.load(f'images/hunt/jump/({i}).png') for i in range(1, 11)]
+
+    walk_images = [pygame.image.load(f'images/hunt/walk/({i}).png') for i in range(1, 11)]
+
+    return attack_images, die_images, idle_images, jump_images, walk_images
+
 
 map_controller = MapController()
-map_controller.create_map([pygame.transform.scale(pygame.image.load(f'images/maps/map1/({i}).png'), (1920/resize, 1080/ resize)) for i in range(1, 5 + 1)], "Forest")
+map_controller.create_map([pygame.transform.scale(pygame.image.load(f'images/maps/map1/({i}).png'), (1920 / resized, 1080 / resized)) for i in range(1, 5 + 1)], "Forest")
+
+hero_controller = HeroController()
+hero_controller.create_hero("Warrior")
+# hero_controller.create_hero("Mage")  # no logic for them yet
+# hero_controller.create_hero("Hunter")  # no logic for them yet
+
 
 menu = Menu()
 actions = Actions()
 
-menu.menu()
+menu.menu()   # must pass objects to this object so that we can see them in the menu
 current_hero = menu.chosen_hero
 
 background_rect = map_controller.show_current_map().get_rect()
