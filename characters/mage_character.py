50fa32484ebf6a96abd1ea9da0c0136410e11021
kumchovylcho <tamerchoyy@gmail.com>
2023-02-12 12:05:43 +0200

    is now inheriting from hero correctly

--- characters/mage_character.py
+++ characters/mage_character.py
@@ -1,31 +1,10 @@
-from pygame import image, transform
+from characters.hero import Hero
 
 
-class Mage:
+class Mage(Hero):
 
-    def __init__(self, x=-30, y=300):
-        self.index = 0
-        self.x = x
-        self.y = y
+    def __init__(self, x: int, y: int, attack_images: list, die_images: list, idle_images: list, jump_images: list,
+                 walk_images: list):
+        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
 
-    # def idle_animation(self, direction):
-    #     self.index += self.__IDLE_SPEED
-    #     if direction == 'right':
-    #         return idle_images_right[int(self.index) % len(idle_images_right)]
-    #     return idle_images_left[int(self.index) % len(idle_images_left)]
-    #
-    # def walk_images(self, direction):
-    #     self.index += self.__IDLE_SPEED
-    #     if direction == "right":
-    #         return walk_images_right[int(self.index) % len(walk_images_right)]
-    #     return walk_images_left[int(self.index) % len(walk_images_left)]
-    #
-    # def jump_animation(self):
-    #     self.index += self.__IDLE_SPEED
-    #     return walk_images_right[int(self.index) % len(walk_images_right)]
-    #
-    # def attack_animation(self, direction):
-    #     self.index += 0.1
-    #     if direction == "right":
-    #         return attack_images_right[int(self.index) % len(attack_images_right)]
-    #     return attack_images_left[int(self.index) % len(attack_images_left)]
+
