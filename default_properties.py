dae34c42b506b3313fd369f3a6c94b304b40c2fd
kumchovylcho <tamerchoyy@gmail.com>
2023-02-13 02:18:02 +0200

    removed pickle and added json since pickle was throwing error after error

--- default_properties.py
+++ default_properties.py
@@ -1,43 +1,24 @@
-import pickle
-from class_maps.map_controller import MapController
-from characters.hunter_character import Hunter
-from characters.mage_character import Mage
-from characters.warrior_character import Warrior
+import json
 
 
 def load_data():
     try:
-        with open("savegame", "rb") as file:
-            take_from_file = pickle.load(file)
+        with open("save_game.json", "r+") as file:
+            game_data = json.load(file)
     except FileNotFoundError:
-        return default_dictionary
-    return take_from_file
+        return default_values
+    return game_data
 
 
-def save_on_close(program_data: dict, map_data: MapController, current_hero: (Mage, Warrior, Hunter)):
-    program_data["Map"].current_map = map_data.current_map
-    program_data[current_hero.__class__.__name__.capitalize()].index = current_hero.index
-
-    with open('savegame', 'wb') as data:
-        pickle.dump(program_data, data)
+def save_on_close(program_data: dict):
+    with open('save_game.json', 'w') as data:
+        json.dump(program_data, data, indent=2)
 
 
-default_dictionary = {
-    "Map": MapController(),      # must fix this dict Tamer
-    "Hunter": Hunter(),
-    "Mage": Mage(),
-    "Warrior": Warrior()
+default_values = {
+    "Map": {'current_map': 0},
+    "Hunter": {'level': 1, 'health': 100},
+    "Mage": {'level': 1, 'health': 100, 'mana': 100},
+    "Warrior": {'level': 1, 'health': 100},
 }
-# mage = Mage()
-# map = MapController()
-# test = load_data()
-#
-# print("##################################################")
-# # print(f"from hunger_character this is the index of the Hunter: {test['Hunter'].index}")
-# print(f"from mage_character this is the index of the Mage: {test['Mage'].index}")
-# # print(f"from hunger_character this is the index of the Warrior: {test['Warrior'].index}")
-# print(f"from map_controller this is the index of the current map: {test['Map'].current_map}")
-# print("##################################################")
-# print(test['Warrior'].jump_animation())
-#
-# save_on_close(default_dictionary, map, mage)
+
