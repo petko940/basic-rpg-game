b2c33e68c3aeed84dc84bb7b8fd992a547659f46
kumchovylcho <tamerchoyy@gmail.com>
2023-02-13 02:17:16 +0200

    added game progress dict, corrected attack logic, heroes are now getting info from json

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import pygame
 from characters.hero_controller import HeroController
 from menu_class import Menu
 from class_maps.map_controller import MapController
-from characters.actions import Actions
+from default_properties import load_data, save_on_close
 
 pygame.init()
 
 
-def loading_game_screen(window, current_map: MapController, hero: object, hero_actions: Actions, rect_of_background: pygame.Rect):
+def loading_game_screen(window, current_map: MapController, hero: object, rect_of_background: pygame.Rect):
     for i in range(510, 0, -1):
         window.blit(current_map.show_current_map(), (0, 0))
-        window.blit(hero.idle_animation("right"), hero_actions.idle())
+        window.blit(hero.idle_animation("right"), hero.idle())  # NOQA
         pygame.draw.rect(window, (50, (255 - i // 2), 0), rect_of_background, int(i * 1.1))
         pygame.display.update()
 
@@ -25,7 +25,7 @@ map_controller = MapController()
 map_controller.create_map([pygame.transform.scale(pygame.image.load(f'images/maps/map1/({i}).png'), (1920 / resized, 1080 / resized)) for i in range(1, 5 + 1)], "Forest")
 
 hero_controller = HeroController()
-hero_controller.create_hero("Warrior", 100, 100)
+hero_controller.create_hero("Warrior", 50, 150)
 hero_controller.create_hero("Mage", 300, 300)
 hero_controller.create_hero("Hunter", 200, 200)
 
@@ -33,9 +33,14 @@ warrior = hero_controller.get_hero_object("Warrior")
 mage = hero_controller.get_hero_object("Mage")
 hunter = hero_controller.get_hero_object("Hunter")
 
+collected_game_info = load_data()
+map_controller.current_map = collected_game_info["Map"]['current_map']
+warrior.level = collected_game_info["Warrior"]['level']
+mage.level = collected_game_info["Mage"]['level']
+hunter.level = collected_game_info["Hunter"]['level']
+
 
 menu = Menu(warrior, mage, hunter)
-actions = Actions()
 
 menu.menu()
 current_hero = menu.chosen_hero
@@ -43,7 +48,7 @@ current_hero = menu.chosen_hero
 
 background_rect = map_controller.show_current_map().get_rect()
 
-loading_game_screen(screen, map_controller, current_hero, actions, background_rect)
+loading_game_screen(screen, map_controller, current_hero, background_rect)
 
 
 is_right = True
@@ -55,6 +60,8 @@ while game_running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 game_running = False
+            elif event.key == pygame.K_SPACE:
+                current_hero.is_attacking = True
             # elif event.key == pygame.K_d:
             #     screen.blit(warrior.walk_images("right"),warrior.idle_animation("right").get_rect())
             # TO DO walk right
@@ -62,28 +69,36 @@ while game_running:
 
     screen.blit(map_controller.show_current_map(), (0, 0))
 
+    if current_hero.is_attacking:
+        screen.blit(current_hero.attack_animation("right"), current_hero.attack())
+
     if pygame.key.get_pressed()[pygame.K_d] and pygame.key.get_pressed()[pygame.K_SPACE]:
-        screen.blit(current_hero.walk_images("right"), actions.walk())
-        screen.blit(current_hero.attack_animation("right"), actions.attack())
+        screen.blit(current_hero.walk_images("right"), current_hero.walk("right"))
+        screen.blit(current_hero.attack_animation("right"), current_hero.attack())
 
     elif pygame.key.get_pressed()[pygame.K_d]:
-        screen.blit(current_hero.walk_images("right"), actions.walk())
-        if actions.check_for_traverse():
+        screen.blit(current_hero.walk_images("right"), current_hero.walk("right"))
+        if current_hero.check_for_traverse():
             map_controller.traverse_image()
         is_right = True
 
     elif pygame.key.get_pressed()[pygame.K_a]:
-        screen.blit(current_hero.walk_images("left"), actions.walk())
+        screen.blit(current_hero.walk_images("left"), current_hero.walk("left"))
         is_right = False
 
-    elif pygame.key.get_pressed()[pygame.K_SPACE]:
-        screen.blit(current_hero.attack_animation("right"), actions.attack())
-
     else:
-        if is_right:
-            screen.blit(current_hero.idle_animation('right'), actions.idle())
-        else:
-            screen.blit(current_hero.idle_animation('left'), actions.idle())
+        if is_right and not current_hero.is_attacking:
+            screen.blit(current_hero.idle_animation('right'), current_hero.idle())
+        elif not is_right and not current_hero.is_attacking:
+            screen.blit(current_hero.idle_animation('left'), current_hero.idle())
     pygame.display.update()
 
 pygame.quit()
+
+progress_of_game = {
+    "Map": {'current_map': map_controller.current_map},
+    "Hunter": {'level': hunter.level, 'health': 100},  # hunter.health
+    "Mage": {'level': mage.level, 'health': 100, 'mana': 100},  # mage.health and mage.mana
+    "Warrior": {'level': warrior.level, 'health': 100},  # warrior.health
+}
+save_on_close(progress_of_game)
