9e7b50ba4fb4db97be84b855d5116aaf860de52b
kumchovylcho <tamerchoyy@gmail.com>
2023-02-14 22:20:38 +0200

    added mana bar as rect object

--- characters/mage_character.py
+++ characters/mage_character.py
@@ -7,4 +7,4 @@ class Mage(Hero):
                  walk_images: list):
         super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
 
-
+        self.mana_bar = self.make_bar(0, 35, self.BAR_LENGTH, 35)
