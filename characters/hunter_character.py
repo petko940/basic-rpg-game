e85976b6d41ab96325da5273394720ab15218834
kumchovylcho <tamerchoyy@gmail.com>
2023-02-14 22:20:33 +0200

    added mana bar as rect object

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -6,3 +6,5 @@ class Hunter(Hero):
     def __init__(self, x: int, y: int, attack_images: list, die_images: list, idle_images: list, jump_images: list,
                  walk_images: list):
         super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
+
+        self.mana_bar = self.make_bar(0, 35, self.BAR_LENGTH, 35)
