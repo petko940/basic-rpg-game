e0aa2bf7427d630caa5fb9e91a893786dc2b4494
kumchovylcho <tamerchoyy@gmail.com>
2023-02-15 02:14:09 +0200

    added health and mana stats

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -8,3 +8,7 @@ class Hunter(Hero):
         super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
 
         self.mana_bar = self.make_bar(0, 35, self.BAR_LENGTH, 35)
+
+        self.max_health = 100
+        self.health = 100
+        self.mana = 80
