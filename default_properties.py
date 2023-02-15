b248be273f62d986baedc930a14cf8f75b0d4f08
kumchovylcho <tamerchoyy@gmail.com>
2023-02-16 00:40:00 +0200

    added more default values

--- default_properties.py
+++ default_properties.py
@@ -17,8 +17,8 @@ def save_on_close(program_data: dict):
 
 default_values = {
     "Map": {'current_map': 0},
-    "Hunter": {'level': 1, 'health': 100},
-    "Mage": {'level': 1, 'health': 100, 'mana': 100},
-    "Warrior": {'level': 1, 'health': 100},
+    "Hunter": {'level': 1, 'health': 100, 'mana': 80},
+    "Mage": {'level': 1, 'health': 80, 'mana': 150},
+    "Warrior": {'level': 1, 'health': 150},
 }
 
