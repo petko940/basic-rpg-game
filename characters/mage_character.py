67e535e518ee7dc44ab2782c3bccf5371adb3f6e
kumchovylcho <tamerchoyy@gmail.com>
2023-02-15 02:14:18 +0200

    added health and mana stats

--- characters/mage_character.py
+++ characters/mage_character.py
@@ -8,3 +8,7 @@ class Mage(Hero):
         super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
 
         self.mana_bar = self.make_bar(0, 35, self.BAR_LENGTH, 35)
+
+        self.max_health = 80
+        self.health = 80
+        self.mana = 150
