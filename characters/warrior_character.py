aeed75fa1b04ad704a2b33e0a9acba5786c2ff2e
kumchovylcho <tamerchoyy@gmail.com>
2023-02-15 02:14:32 +0200

    added health stat

--- characters/warrior_character.py
+++ characters/warrior_character.py
@@ -7,4 +7,5 @@ class Warrior(Hero):
                  walk_images: list):
         super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
 
-
+        self.max_health = 150
+        self.health = 150
