a1efaa1661c6fc3c48a4a979595d8c5715f0103b
kumchovylcho <tamerchoyy@gmail.com>
2023-02-16 14:51:40 +0200

    now inherits profile picture

--- characters/mage_character.py
+++ characters/mage_character.py
@@ -4,10 +4,10 @@ from characters.hero import Hero
 class Mage(Hero):
 
     def __init__(self, x: int, y: int, attack_images: list, die_images: list, idle_images: list, jump_images: list,
-                 walk_images: list):
-        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
+                 walk_images: list, profile_pic: object):
+        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images, profile_pic)
 
-        self.mana_bar = self.make_bar(0, 35, self.BAR_LENGTH, 35)
+        self.mana_bar = self.make_bar(self.frame.width, 35, self.BAR_LENGTH, 35)
 
         self.max_health = 80
         self.health = 80
