46f2039bfebcb52aefb2e4bb043f5d96b0f68e15
kumchovylcho <tamerchoyy@gmail.com>
2023-02-16 14:51:48 +0200

    now inherits profile picture

--- characters/warrior_character.py
+++ characters/warrior_character.py
@@ -4,8 +4,8 @@ from characters.hero import Hero
 class Warrior(Hero):
 
     def __init__(self, x: int, y: int, attack_images: list, die_images: list, idle_images: list, jump_images: list,
-                 walk_images: list):
-        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images)
+                 walk_images: list, profile_pic: object):
+        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images, profile_pic)
 
         self.max_health = 150
         self.health = 150
