739add68a1ed6ab412b36a4b958bb99b318a1d99
kumchovylcho <tamerchoyy@gmail.com>
2023-02-25 19:47:30 +0200

    die image is now single image

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -4,9 +4,9 @@ from characters.skills import HealAndMana
 
 class Hunter(Hero):
 
-    def __init__(self, x: int, y: int, attack_images: list, die_images: list, idle_images: list, jump_images: list,
+    def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
-        super().__init__(x, y, attack_images, die_images, idle_images, jump_images, walk_images, profile_pic)
+        super().__init__(x, y, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
 
         self.mana_bar = self.make_bar(self.frame.width, 35, self.BAR_LENGTH, 35)
 
