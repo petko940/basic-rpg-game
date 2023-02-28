4700b587a3047d169a406fe560158c764b66e8b4
kumchovylcho <tamerchoyy@gmail.com>
2023-02-28 14:16:18 +0200

    health and max health are now stored in Hero

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -3,15 +3,13 @@ from characters.skills import HealAndMana, ArrowRain
 
 
 class Hunter(Hero):
+    START_HEALTH = 100
 
     def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
-        super().__init__(x, y, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
+        super().__init__(x, y, self.START_HEALTH, self.START_HEALTH, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
 
         self.mana_bar = self.make_bar(self.frame.width, 35, self.BAR_LENGTH, 35)
-
-        self.max_health = 100
-        self.health = 100
 
         self.max_mana = 80
         self.mana = 80
