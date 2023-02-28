4a7a8cecb82b55b4f55aacbffeb4cf4806f53438
kumchovylcho <tamerchoyy@gmail.com>
2023-02-28 14:16:33 +0200

    health and max health are now stored in Hero

--- characters/warrior_character.py
+++ characters/warrior_character.py
@@ -3,13 +3,11 @@ from characters.skills import AxeBasicAttack, Heal, DamageBoost, PassiveCrit
 
 
 class Warrior(Hero):
+    START_HEALTH = 150
 
     def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
-        super().__init__(x, y, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
-
-        self.max_health = 150
-        self.health = 150
+        super().__init__(x, y, self.START_HEALTH, self.START_HEALTH, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
 
         axe_basic_attack = AxeBasicAttack()
         heal = Heal()
