cba6ec92da98e5a75587f260e4128b04ec2ce447
kumchovylcho <tamerchoyy@gmail.com>
2023-03-03 19:26:38 +0200

    added arrow shot to hunter skills dictionary

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -1,5 +1,5 @@
 from characters.hero import Hero
-from characters.skills import HealAndMana, ArrowRain
+from characters.skills import HealAndMana, ArrowRain, ArrowShot
 
 
 class Hunter(Hero):
@@ -14,11 +14,12 @@ class Hunter(Hero):
         self.max_mana = 80
         self.mana = 80
 
+        arrow_shot = ArrowShot(5)
         hp_mp_gain = HealAndMana()
         arrow_rain = ArrowRain(40)
 
         self.skills = {
-            1: 'first skill',
+            1: arrow_shot,
             2: hp_mp_gain,
             3: 'third skill',
             4: arrow_rain
@@ -40,8 +41,6 @@ class Hunter(Hero):
 
     def increase_mana_bar_width(self, heal_power: int or float):
         self.mana_bar.width = self.increase_bar_width(self.mana, self.max_mana, heal_power)
-        self.receive_mana(heal_power)
 
     def decrease_mana_bar_width(self, skill_cost: int or float):
         self.mana_bar.width = self.lower_bar_width(self.mana, self.max_mana, skill_cost)
-        self.consume_mana_on_skill(skill_cost)
