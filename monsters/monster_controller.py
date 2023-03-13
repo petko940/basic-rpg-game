ef7f84d5e9c34a2199ef601dedc5ba8fc50c2ad1
kumchovylcho <tamerchoyy@gmail.com>
2023-03-14 01:55:18 +0200

    added chase player method

--- monsters/monster_controller.py
+++ monsters/monster_controller.py
@@ -1,4 +1,5 @@
 from typing import List
+import pygame
 from monsters.monster import Monster
 
 
@@ -6,10 +7,26 @@ class MonsterController:
 
     def __init__(self, *monsters: Monster):
         self.monsters: List[Monster] = [*monsters]
+        self.first_spawn = False
+
+    @property
+    def current_monster(self):
+        return self.monsters[0]
 
     def spawn_monster(self):
-        monster = self.monsters[0]
+        monster = self.current_monster
+
+        if not self.first_spawn:
+            self.first_spawn = True
 
         if monster.can_spawn_monster():
             monster.increase_monsters_on_screen()
 
+    def chase_player(self, screen, player):
+        monster = self.current_monster
+
+        if monster.is_dead:
+            return
+
+        image = monster.walk(player.x)
+        screen.blit(image, monster.monster_position())
