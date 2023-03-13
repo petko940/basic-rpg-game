b7d498cfc6e14ddc5e0dfeb493221251874f082a
kumchovylcho <tamerchoyy@gmail.com>
2023-03-14 01:54:06 +0200

    added several methods and added health attributes

--- monsters/monster.py
+++ monsters/monster.py
@@ -4,16 +4,20 @@ from abc import ABC, abstractmethod
 class Monster(ABC):
     MONSTERS_ON_SCREEN_LIMIT = 1
 
-    def __init__(self, x_pos: int, y_pos: int):
+    HEALTH_GAIN_AFTER_DEATH = 50
+
+    def __init__(self, health: int, x_pos: int, y_pos: int):
         self.monsters_on_screen = 0
 
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.health = health
+        self.max_health = health
 
         self.walk_and_idle_index = 0
 
-        self.first_spawn = False
         self.left_direction = True
+        self.target_reached = False
 
     @abstractmethod
     def idle(self):
@@ -31,6 +35,10 @@ class Monster(ABC):
     def attack(self):
         pass
 
+    @property
+    def is_dead(self):
+        return self.health <= 0
+
     def monster_position(self):
         return self.x_pos, self.y_pos
 
@@ -47,3 +55,8 @@ class Monster(ABC):
     def increase_monsters_on_screen(self):
         if self.can_spawn_monster():
             self.monsters_on_screen += 1
+
+    def power_up_after_death(self):
+        self.max_health += Monster.HEALTH_GAIN_AFTER_DEATH
+        self.health = self.max_health
+        self.left_direction = True
