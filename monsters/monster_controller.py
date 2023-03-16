2ca498b8f0fce4fb745b10b1b36472c566f2d912
kumchovylcho <tamerchoyy@gmail.com>
2023-03-16 03:00:01 +0200

    added display health bar method

--- monsters/monster_controller.py
+++ monsters/monster_controller.py
@@ -1,5 +1,5 @@
 from typing import List
-import pygame
+from pygame import draw, Rect
 from monsters.monster import Monster
 
 
@@ -13,9 +13,10 @@ class MonsterController:
     def current_monster(self):
         return self.monsters[0]
 
-    @property
-    def target_reached(self):
-        return self.current_monster.target_reached
+    def display_health_bar(self, screen):
+        if not self.current_monster.is_dead:
+            draw.rect(screen, self.current_monster.HEALTH_BAR_PAD_COLOR, self.current_monster.health_bar_pad_position())
+            draw.rect(screen, self.current_monster.HEALTH_BAR_COLOR, self.current_monster.health_bar_position())
 
     def spawn_monster(self):
         monster = self.current_monster
@@ -29,7 +30,7 @@ class MonsterController:
     def chase_player(self, screen, player):
         monster = self.current_monster
 
-        if self.target_reached or monster.is_dead or monster.is_attacking:
+        if monster.is_dead or monster.is_attacking:
             return
 
         image = monster.walk(player.x)
@@ -38,10 +39,15 @@ class MonsterController:
     def stay_idle(self, screen):
         monster = self.current_monster
 
-        if self.target_reached and not monster.is_attacking and not monster.is_dead:
+        if not monster.is_attacking and not monster.is_dead:
             image = monster.idle()
             screen.blit(image, monster.monster_position())
 
     def actions(self, screen, player):
-        self.chase_player(screen, player)
+        draw.rect(screen, (255, 0, 0), self.current_monster.rect_hit_box, 3)
+
+        if not self.current_monster.check_target_reached(player.x):
+            self.chase_player(screen, player)
+            return
+
         self.stay_idle(screen)
