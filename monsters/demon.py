f7b209ef4ea98d5eb6d656e69c01ccbbace1c3d0
kumchovylcho <tamerchoyy@gmail.com>
2023-03-16 02:58:50 +0200

    added new methods

--- monsters/demon.py
+++ monsters/demon.py
@@ -1,10 +1,12 @@
 from monsters.monster import Monster
-from pygame import image, transform
+from pygame import image, transform, Rect
 
 
 class Demon(Monster):
     IMAGE_LOOP_SPEED = 0.12
     MOVE_SPEED = 2
+
+    LOWER_HIT_BOX = 120
 
     def __init__(self, health: int, x_pos: int, y_pos: int):
         super().__init__(health, x_pos, y_pos)
@@ -21,36 +23,41 @@ class Demon(Monster):
         self.walk_left = [transform.scale(image.load(f'monsters/demon_sprites/walk/{i}.png'), (338, 325)) for i in range(6)]
         self.walk_right = [transform.flip(self.walk_left[i], True, False) for i in range(len(self.walk_left))]
 
+        self.rect_image = self.attack_left[0].get_rect()
+
+    @property
+    def rect_hit_box(self):
+        if self.left_direction:
+            return Rect(self.x_pos + Demon.LOWER_HIT_BOX, self.y_pos + 30, self.rect_image.width - Demon.LOWER_HIT_BOX, self.rect_image.height)
+        return Rect(self.x_pos, self.y_pos + 30, self.rect_image.width - Demon.LOWER_HIT_BOX, self.rect_image.height)
+
     def attack(self):
         pass
 
     def check_target_reached(self, hero_x_pos: int):
-        difference_left = abs((self.x_pos - 120) - hero_x_pos)
-        difference_right = abs((self.x_pos + 120) - hero_x_pos)
+        difference_left = abs((self.x_pos - Demon.LOWER_HIT_BOX) - hero_x_pos)
+        difference_right = abs((self.x_pos + Demon.LOWER_HIT_BOX) - hero_x_pos)
 
-        if difference_left < 5 or difference_right < 5:
-            self.target_reached = True
-        else:
-            self.target_reached = False
+        if difference_left < 5 or difference_right < 5 or difference_left + difference_right == Demon.LOWER_HIT_BOX * 2:
+            return True
+        return False
 
     def walk(self, hero_x_pos: int):
-        """
-        the number 120 are the empty pixels between the hero and the end of the image
-        """
         self.walk_and_idle_index += Demon.IMAGE_LOOP_SPEED
 
         image_index = int(self.walk_and_idle_index) % len(self.walk_left)
 
-        self.check_target_reached(hero_x_pos)
+        if self.x_pos - Demon.LOWER_HIT_BOX > hero_x_pos:
+            self.x_pos -= Demon.MOVE_SPEED
+            self.health_bar.x -= Demon.MOVE_SPEED
+            self.background_health_bar.x -= Demon.MOVE_SPEED
+            self.left_direction = True
 
-        if not self.target_reached:
-            if self.x_pos - 120 > hero_x_pos:
-                self.x_pos -= Demon.MOVE_SPEED
-                self.left_direction = True
-
-            elif hero_x_pos > self.x_pos + 120:
-                self.x_pos += Demon.MOVE_SPEED
-                self.left_direction = False
+        elif hero_x_pos > self.x_pos + Demon.LOWER_HIT_BOX:
+            self.x_pos += Demon.MOVE_SPEED
+            self.health_bar.x += Demon.MOVE_SPEED
+            self.background_health_bar.x += Demon.MOVE_SPEED
+            self.left_direction = False
 
         return self.get_image(image_index, self.walk_left, self.walk_right)
 
@@ -68,3 +75,13 @@ class Demon(Monster):
         if self.left_direction:
             return images_left[index]
         return images_right[index]
+
+    def health_bar_position(self):
+        if self.left_direction:
+            return self.health_bar.x + 200, self.y_pos + 30, self.health_bar.width, self.HEALTH_BAR_HEIGHT
+        return self.health_bar.x, self.y_pos + 30, self.health_bar.width, self.HEALTH_BAR_HEIGHT
+
+    def health_bar_pad_position(self):
+        if self.left_direction:
+            return self.background_health_bar.x + 200, self.y_pos + 30, self.background_health_bar.width, self.HEALTH_BAR_HEIGHT
+        return self.background_health_bar.x, self.y_pos + 30, self.background_health_bar.width, self.HEALTH_BAR_HEIGHT
