3e6fe7cc14eab5bd2dbc5dedbae93eb8d65f224b
kumchovylcho <tamerchoyy@gmail.com>
2023-03-20 01:24:21 +0200

    removed optional parameter

--- class_maps/all_maps/forest.py
+++ class_maps/all_maps/forest.py
@@ -3,5 +3,5 @@ from class_maps.all_maps.map import Map
 
 class Forest(Map):
 
-    def __init__(self, name: str, images: list, index=0):
-        super().__init__(name, images, index)
+    def __init__(self, name: str, images: list):
+        super().__init__(name, images)
