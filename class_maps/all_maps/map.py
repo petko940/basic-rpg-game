8bd937bae1eb68f7fb423e177d6a1d63ba94ec82
kumchovylcho <tamerchoyy@gmail.com>
2023-03-20 01:24:28 +0200

    removed optional parameter

--- class_maps/all_maps/map.py
+++ class_maps/all_maps/map.py
@@ -4,11 +4,11 @@ from abc import ABC
 class Map(ABC):
     MAP_WIDTH = 1366
 
-    def __init__(self, name: str, images: list, index=0):
+    def __init__(self, name: str, images: list):
         self.name = name
         self.images = images
-        self.index = index
-        self.cleared_stages = {stage: False if stage != 0 else True for stage in range(len(self.images))}
+        self.index = 0
+        self.cleared_stages = {stage: False for stage in range(len(self.images))}
 
     @property
     def index(self):
@@ -39,6 +39,12 @@ class Map(ABC):
 
     def get_image_index(self):
         return self.index
+
+    def last_image(self):
+        return self.index == len(self.images) - 1
+
+    def first_image(self):
+        return self.index == 0
 
     def get_map_width(self):
         return self.MAP_WIDTH
