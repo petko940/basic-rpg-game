b22472407ec978d5b04fc2e133097a8549f81b0a
kumchovylcho <tamerchoyy@gmail.com>
2023-03-21 02:07:49 +0200

    added get stronger method

--- characters/warrior_character.py
+++ characters/warrior_character.py
@@ -5,6 +5,8 @@ from characters.skills import AxeBasicAttack, Heal, DamageBoost, PassiveCrit
 class Warrior(Hero):
     START_HEALTH = 150
 
+    HEALTH_INCREASE_PER_LEVEL = 50
+
     def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
         super().__init__(x, y, self.START_HEALTH, self.START_HEALTH, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
@@ -20,3 +22,11 @@ class Warrior(Hero):
             3: damage_boost,
             4: passive_crit
         }
+
+    def get_stronger_after_level_up(self):
+        self.max_health += self.HEALTH_INCREASE_PER_LEVEL
+        self.health = self.max_health
+        self.health_bar.width = self.BAR_LENGTH
+        for skill in self.skills.values():
+            if self.level >= skill.LEVEL_REQUIRED:
+                skill.level_up()
