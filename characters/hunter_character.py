8d43ffed22077c9eb10ffa5f8c49913f8d1b386e
kumchovylcho <tamerchoyy@gmail.com>
2023-03-21 02:04:22 +0200

    added get_stronger method and nerfed mana costs

--- characters/hunter_character.py
+++ characters/hunter_character.py
@@ -7,6 +7,9 @@ class Hunter(Hero):
 
     MANA_REGEN_PER_SECOND = 1
 
+    HEALTH_INCREASE_PER_LEVEL = 40
+    MANA_INCREASE_PER_LEVEL = 25
+
     def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
         super().__init__(x, y, self.START_HEALTH, self.START_HEALTH, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
@@ -18,7 +21,7 @@ class Hunter(Hero):
 
         arrow_shot = ArrowShot(5)
         hp_mp_gain = HealAndMana()
-        rapid_shot = RapidShot(15)
+        rapid_shot = RapidShot(10)
         arrow_rain = ArrowRain(40)
 
         self.skills = {
@@ -27,6 +30,17 @@ class Hunter(Hero):
             3: rapid_shot,
             4: arrow_rain
         }
+
+    def get_stronger_after_level_up(self):
+        self.max_health += self.HEALTH_INCREASE_PER_LEVEL
+        self.health = self.max_health
+        self.max_mana += self.MANA_INCREASE_PER_LEVEL
+        self.mana = self.max_mana
+        self.health_bar.width = self.BAR_LENGTH
+        self.mana_bar.width = self.BAR_LENGTH
+        for skill in self.skills.values():
+            if self.level >= skill.LEVEL_REQUIRED:
+                skill.level_up()
 
     def check_mana_limit(self):
         if self.mana > self.max_mana:
