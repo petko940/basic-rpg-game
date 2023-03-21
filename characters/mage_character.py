68cf0d24c175bf77e60cebaf4c618ef2e882cef2
kumchovylcho <tamerchoyy@gmail.com>
2023-03-21 02:05:02 +0200

    added get_stronger method and nerfed mana costs

--- characters/mage_character.py
+++ characters/mage_character.py
@@ -7,6 +7,9 @@ class Mage(Hero):
 
     MANA_REGEN_PER_SECOND = 1
 
+    HEALTH_INCREASE_PER_LEVEL = 30
+    MANA_INCREASE_PER_LEVEL = 35
+
     def __init__(self, x: int, y: int, attack_images: list, die_image, idle_images: list, jump_images: list,
                  walk_images: list, profile_pic: object):
         super().__init__(x, y, self.START_HEALTH, self.START_HEALTH, attack_images, die_image, idle_images, jump_images, walk_images, profile_pic)
@@ -16,9 +19,9 @@ class Mage(Hero):
         self.max_mana = 150
         self.mana = 150
 
-        blue_ball = BlueBall(20)
+        blue_ball = BlueBall(10)
         hp_mp_gain = HealAndMana()
-        lightning = Lightning(30)
+        lightning = Lightning(24)
         meteor_strike = MeteorStrike(40)
 
         self.skills = {
@@ -27,6 +30,17 @@ class Mage(Hero):
             3: lightning,
             4: meteor_strike,
         }
+
+    def get_stronger_after_level_up(self):
+        self.max_health += self.HEALTH_INCREASE_PER_LEVEL
+        self.health = self.max_health
+        self.max_mana += self.MANA_INCREASE_PER_LEVEL
+        self.mana = self.max_mana
+        self.health_bar.width = self.BAR_LENGTH
+        self.mana_bar.width = self.BAR_LENGTH
+        for skill in self.skills.values():
+            if self.level >= skill.LEVEL_REQUIRED:
+                skill.level_up()
 
     def check_mana_limit(self):
         if self.mana > self.max_mana:
