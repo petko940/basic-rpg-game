018b6e41c44528decda453e6566fce665d6438ba
kumchovylcho <tamerchoyy@gmail.com>
2023-03-25 02:38:01 +0200

    deleted unused files

--- timer.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import time
-
-
-def timer(start_time, screen):
-    font = pygame.font.SysFont(None, 100)
-    elapsed_time = time.time() - start_time
-    timer_text = font.render(str(round(elapsed_time, 2)), True, (255, 255, 255))
-    screen.blit(timer_text, (300, 300))
