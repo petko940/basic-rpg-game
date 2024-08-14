a36000c05cf1c4e2dc2b354c3f540fb30db58c65
Tamer <tamerchoyy@gmail.com>
2024-08-14 03:01:34 +0300

    added util functions

--- utils.py
+++ utils.py
@@ -1,2 +1,20 @@
+import pygame as pg
+
+
 def get_screen_size():
     return 1366, 768
+
+
+def create_font(value:str, font:str, size_font:int, colour: tuple, bold=True, antialias=True):
+    font = pg.font.SysFont(font, size_font, bold)
+    return font.render(value, antialias, colour)
+
+
+def numbers_format(num):
+    num = float(f'{num:.3g}')
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    result = f"{str(num).rstrip('0').rstrip('.')}{['', 'K', 'M', 'B', 'T'][magnitude]}"
+    return result
